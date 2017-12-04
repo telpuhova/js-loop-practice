2fb5475c285a1c0b42eb47edc294e80b955690f4
Byron Chang and Natalia Telpukhova <byron@>
2017-12-04 09:47:45 -0800

    add loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
+  // $("h1").click(function() {
+  //   alert("This is a header.");
+  // });
+  //
+  // $("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+  //
+  // $("img").click(function() {
+  //   alert("This is an image.");
+  // });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
+  var tags = ["h1", "p", "img"];
+  tags.forEach(function(tag) {
+    $(tag).click(function() {
+      alert("This is a " + tag);
+    });
   });
 });
